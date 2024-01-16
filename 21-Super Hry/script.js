e42fda3b3eb0147189f586415affa1fbc2dd68c5
StromYTB <145440259+StromYTB@users.noreply.github.com>
2024-01-16 12:56:50 +0100

    Delete 21-Super Hry directory

--- 21-Super Hry/script.js
+++
@@ -1,7 +0,0 @@
-function travel(){
-    window.location.href = 'http://127.0.0.1:5500/21-Super%20Hry/piškvorky.html';
-}
-
-function travelTwo(){
-    window.location.href = 'http://127.0.0.1:5500/21-Super%20Hry/match.html';
-}
