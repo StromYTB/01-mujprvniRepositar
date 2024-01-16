4bf53f907d1f6b62ec2f49e130e06d8ff69be8ec
StromYTB <145440259+StromYTB@users.noreply.github.com>
2024-01-16 12:57:25 +0100

    Add files via upload

---
+++ 21-Super Hry/script.js
@@ -0,0 +1,7 @@
+function travel(){
+    window.location.href = 'http://127.0.0.1:5500/21-Super%20Hry/piškvorky.html';
+}
+
+function travelTwo(){
+    window.location.href = 'http://127.0.0.1:5500/21-Super%20Hry/match.html';
+}
